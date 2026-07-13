678462899bbd48faca5bbdf3d9f3547cfb82dc4d
KADENPC\kanuc <Kanuchos@gmail.com>
2026-07-13 18:54:04 -0500

    added inputs x & y

--- Roll a Ball/Assets/Scripts/Ballmovement.cs
+++ Roll a Ball/Assets/Scripts/Ballmovement.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Ballmovement : MonoBehaviour
 {
+    float inputX;
+    float inputY;
     public int Speed = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,4 +17,15 @@ public class Ballmovement : MonoBehaviour
     {
         
     }
+
+    void OnMove (InputValue moveValue)
+    {
+        Vector2 moveVector = moveValue.Get<Vector2>();
+
+        inputX = moveVector.x;
+        inputY = moveVector.y;
+
+    }
+
 }
+
